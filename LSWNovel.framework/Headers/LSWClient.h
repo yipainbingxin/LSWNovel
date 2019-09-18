23dc0499cc07c011b7957a4b5dd294f5bb1e2150
 + <472331078@qq.com>
2019-09-18 11:53:17 +0800

    将framework和resource提交到本地库

---
+++ LSWNovel.framework/Headers/LSWClient.h
@@ -0,0 +1,20 @@
+//
+//  LSWClient.h
+//  LSWNovel
+//
+//  Created by bing on 2019/9/18.
+//  Copyright © 2019 youshu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSWClient : NSObject
++ (void)initWithAppkey:(NSString *)appKey withAppSecret:(NSString *)appSecret;
++ (void)presetHomeVC:(UIViewController*) homeClass;
+- (void)dismissHomeVC;
+
+@end
+
+NS_ASSUME_NONNULL_END
