266aad39d04537dbf09e99f4cbf06f8bc5998182
 + <472331078@qq.com>
2019-09-23 16:14:07 +0800

    提交本地代码

--- LSWNovel.framework/Headers/LSWClient.h
+++ LSWNovel.framework/Headers/LSWClient.h
@@ -11,7 +11,24 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface LSWClient : NSObject
-+ (void)initWithAppkey:(NSString *)appKey withAppSecret:(NSString *)appSecret withUserCode:(NSString*)user_code;
+
+/**
+ 初始化乐书屋小说组件
+ @param appid 开发者联系商务获取您的appid
+ @param appSecret 开发者联系商务获取您的appSecret
+ */
++ (void)initWithAppid:(NSString *)appid withAppSecret:(NSString *)appSecret;
+
+/**
+ 设置用户标识
+ @param user_code 用户ID
+ */
++ (void)initUsercode:(NSString*)user_code;
+
+/**
+ push出乐书屋小说
+ @param homeClass 当前的导航控制前
+ */
 + (void)presetHomeVC:(UINavigationController*) homeClass;
 
 
